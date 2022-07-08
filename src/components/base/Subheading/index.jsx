bb10dc3eb87baf2506d8e170a2f62224d725c41e
imrancs058 <imrancs058@yahoo.com>
2022-07-08 18:35:00 +0500

    delete old files

--- src/components/base/Subheading/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import * as styles from "./styles.module.scss"
-
-const Subheading = ({ title }) => {
-	return <h3 className={styles.subHeading}>{title}</h3>
-}
-
-export default Subheading
